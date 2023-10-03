dd2cce0000b0bada62d7eb406d3a44d4edc92d62
Баскаева Гелена <baskaevagelena@MacBook-Air-Baskaeva.local>
2023-10-03 19:06:38 +0300

    Initial commit

---
+++ src/leson.java
@@ -0,0 +1,15 @@
+public class leson {
+public static void main (String[]args){
+    int varInt = 25;
+    byte vatByte = -5;
+    short varShot = 11;
+    long varLong = 1200L;
+    float varFloat = 11.7F;
+    double varDouble = 34.67;
+    char varChar = 2;
+    boolean varBoolean = true;
+
+
+
+}
+}
