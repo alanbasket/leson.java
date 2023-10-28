124d470f910bc7b531086e121225b9ee6681dd6c
baskaevalan <alanbasskaev002@gmail.com>
2023-10-28 16:46:20 +0300

    сделал задачу 1.2.6

---
+++ src/Programmersandmonitora.java
@@ -0,0 +1,11 @@
+public class Programmersandmonitora {
+    public static void main(String[] args) {
+        drawisMonitorsCounter(12,6);
+    }
+
+    public static void drawisMonitorsCounter(int monitors, int programmers) {
+
+        int remains = monitors * programmers;
+        System.out.println(remains);
+    }
+}
